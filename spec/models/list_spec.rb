73b5e707c25c6e9bc416f0bb2ccb0eb5465ed6aa
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-10-24 21:02:30 -0500

    refactoring

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe List, type: :model do
   describe '#complete_all_tasks!' do
     it 'should return true if all tasks are completed' do
-      list = List.new(name: "chores")
-      task1 = Task.new(list_id: list.id, complete: false)
-      task2 = Task.new(list_id: list.id, complete: false)
+      list = List.create(name: "chores")
+      task1 = Task.create(list_id: list.id, complete: false)
+      task2 = Task.create(list_id: list.id, complete: false)
 
       list.complete_all_tasks!
       list.tasks.each do |task|
@@ -17,9 +17,9 @@ RSpec.describe List, type: :model do
   describe '#snooze_all_tasks!' do
     it 'should add 1 hour to all tasks' do
       now_time_object = Time.now
-      list = List.new(name: "chores")
-      task1 = Task.new(list_id: list.id, deadline: now_time_object)
-      task2 = Task.new(list_id: list.id, deadline: now_time_object)
+      list = List.create(name: "chores")
+      task1 = Task.create(list_id: list.id, deadline: now_time_object)
+      task2 = Task.create(list_id: list.id, deadline: now_time_object)
 
       list.snooze_all_tasks!
       list.tasks.each do |task|
@@ -30,42 +30,34 @@ RSpec.describe List, type: :model do
 
   describe '#total_duration' do
     it 'should add up all hours of tasks' do
-      list = List.new(name: "chores")
-      task1 = Task.new(list_id: list.id, duration: 60)
-      task2 = Task.new(list_id: list.id, duration: 10)
+      list = List.create(name: "chores")
+      task1 = Task.create(list_id: list.id, duration: 60)
+      task2 = Task.create(list_id: list.id, duration: 10)
 
-      list.total_duration
-      list.tasks.each do |task|
-        expect(task.duration).to eq(70)
-      end
+      expect(list.total_duration).to eq(70)
     end
   end
 
   describe '#incomplete_tasks' do
     it 'should list all tasks that are incomplete' do
-      list = List.new(name: "chores")
-      task1 = Task.new(list_id: list.id, complete: false)
-      task2 = Task.new(list_id: list.id, complete: true)
-      task3 = Task.new(list_id: list.id, complete: true)
+      list = List.create(name: "chores")
+      Task.create(list_id: list.id, complete: false)
+      Task.create(list_id: list.id, complete: true)
+      Task.create(list_id: list.id, complete: true)
+      Task.create(list_id: list.id, complete: false)
 
-      list.incomplete_tasks
-      list.tasks.each do |task|
-        expect(task.incomplete_tasks).to eq(2)
-      end
+      expect(list.incomplete_tasks.count).to eq(2)
     end
   end
 
   describe '#favorite_tasks' do
     it 'should only return tasks with a favorite of true' do
-     list = List.new(name: "chores")
-     task1 = Task.new(list_id: list.id, favorite: false)
-     task2 = Task.new(list_id: list.id, favorite: true)
-     task3 = Task.new(list_id: list.id, favorite: true)
+     list = List.create(name: "chores")
+     task1 = Task.create(list_id: list.id, favorite: false)
+     task2 = Task.create(list_id: list.id, favorite: true)
+     task3 = Task.create(list_id: list.id, favorite: true)
 
-     list.favorite_tasks
-     list.tasks.each do |task|
-      expect(task.favorite_tasks).to eq(2)
-     end
+     expect(list.favorite_tasks.count).to eq(2)
     end
   end
 end
